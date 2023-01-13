ee66d9a8a2d3e13ee6a59055ac785af4906070ea
Richard.I <114187082+RichardIvanchevski@users.noreply.github.com>
2023-01-13 17:00:24 +0200

    java

---
+++ zad7.5_JAVA/ResolveUrlToIP.java
@@ -0,0 +1,27 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ResolveUrlToIP {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter hostname to find IP address");
+        String hostname = scanner.nextLine();
+
+        try {
+            inet = InetAddress.getByName(hostname);  
+        } catch (UnknownHostException e) {
+            System.out.println("Error while trying to resolve the given URL");
+        }
+       
+        String IPAddress = null;
+        if (inet != null) {
+            IPAddress = inet.getHostAddress();
+        }
+        System.out.printf("IP address of host %s  is %s %n", hostname, IPAddress);
+    }
+}
+
+
+
